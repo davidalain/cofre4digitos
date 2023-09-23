63454dc0e51f04c0726d93bd7a3ed202d9f2763d
Samuel Ferraz <ssf@discente.ifpe.edu.br>
2023-09-23 23:59:00 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
  */
 
 /*
- * @author Samuel Ferraz
+ * @author Samuel de Souza Ferraz
  * @date 2023-09-23
  * @license MIT
  */
 
+
 #include "mbed.h"
+#include <chrono>
+
+using namespace std::chrono;
 
 /** Sistema embarcado de cofre de 4 dígitos numéricos pré-programados.
  */
@@ -17,28 +21,56 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-// Pinos digitais para ler botões
-InterruptIn button0(PA_3);
-InterruptIn button1(D1);
-InterruptIn button2(D2);
-InterruptIn button3(D3);
-InterruptIn button4(D4);
-InterruptIn button5(D5);
-InterruptIn button6(D6);
-InterruptIn button7(D7);
-InterruptIn button8(D8);
-InterruptIn button9(D9);
-InterruptIn buttonClear(D10);
-InterruptIn buttonOK(D11);
-
 // Pinos digitais para acionar LEDs
-BusOut display(PA_3, PA_2, PA_10, PB_3, PB_5, PB_4);
+DigitalOut led1(PA_8); 
+DigitalOut led2(PB_5); 
+DigitalOut led3(PB_6); 
+DigitalOut led4(PB_10); 
+
+DigitalOut correctLED(PB_3);
+DigitalOut wrongLED(PA_10);
+
+// Pinos digitais para ler botões
+InterruptIn btn0(PB_8); 
+InterruptIn btn1(PB_9);
+InterruptIn btn2(PA_5);
+InterruptIn btn3(PA_6);
+InterruptIn btn4(PA_7);
+InterruptIn btn5(PC_7);
+InterruptIn btn6(PA_9);
+InterruptIn btn7(PB_4);
+InterruptIn btn8(PA_2);
+InterruptIn btn9(PA_3);
+InterruptIn btnClear(PB_2);
+InterruptIn btnOk(PC_4);
+
 
 // Senha do sistema. Armazenada em uma variável global do tipo inteiro sem sinal.
 unsigned int password = 2023;
 
+void ValidatePassword(){
+
+}
+
+void Clear(){
+
+}
+
+
 int main()
 {
+    btn0.rise(&ValidatePassword);
+    btn1.rise(&ValidatePassword);
+    btn2.rise(&ValidatePassword);
+    btn3.rise(&ValidatePassword);
+    btn4.rise(&ValidatePassword);
+    btn5.rise(&ValidatePassword);
+    btn7.rise(&ValidatePassword);
+    btn7.rise(&ValidatePassword);
+    btn8.rise(&ValidatePassword);
+    btn9.rise(&ValidatePassword);
+    btnClear.rise(&ValidatePassword);
+    btnOk.rise(&Clear);
 
     while (true) {
         ThisThread::sleep_for(BLINKING_RATE);
